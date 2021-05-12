415fb24bdaeda596ee8b94a2753f65fc44ef7145
github username <scott.gloyna01@gmail.com>
2021-05-12 13:04:11 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Pokemon.destroy_all
+
+pokemons_json = RestClient.get "https://pokeapi.co/api/v2/pokemon"
+
+pokemons= JSON.parse(pokemons_json)["results"]
+
+
+pokemons.each do |pokemon| 
+    newPokemon = Pokemon.new
+    newPokemon.name = pokemon["name"]
+    newPokemon.url = pokemon["url"]
+    newPokemon.save
+end
